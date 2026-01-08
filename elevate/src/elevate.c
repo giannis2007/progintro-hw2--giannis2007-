cec150c5faa554ad7ccd533112e60fd018c978e6
gvithoulkas <johnvyth2007@gmail.com>
2026-01-08 15:32:24 +0200

    Update elevate.c

--- elevate/src/elevate.c
+++ elevate/src/elevate.c
@@ -1,6 +1,65 @@
-#include <stdio.h>
+// elevate.c
+// Reference: HW2 Elevator Problem
+// Author: <Your Name>
+// Date: <Date>
 
-int main() {
-    printf("Elevator project started!\n");
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "elevate.h"
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s <inputfile> [--mode=recurse|brute|memoize|dp]\n", argv[0]);
+        return 1;
+    }
+
+    char *inputFile = argv[1];
+    char *mode = "dp";   // default behavior (όπως λέει η εκφώνηση)
+
+    if (argc >= 3 && strncmp(argv[2], "--mode=", 7) == 0) {
+        mode = argv[2] + 7;
+    }
+
+    FILE *file = fopen(inputFile, "r");
+    if (!file) {
+        fprintf(stderr, "Cannot open input file %s\n", inputFile);
+        return 1;
+    }
+
+    int numPeople, numStops;
+    fscanf(file, "%d", &numPeople);
+
+    int dests[numPeople];
+    for (int i = 0; i < numPeople; i++) {
+        fscanf(file, "%d", &dests[i]);
+    }
+
+    fscanf(file, "%d", &numStops);
+    fclose(file);
+
+    int lastStop = 0;
+    int minCost = 0;
+
+    if (strcmp(mode, "recurse") == 0) {
+        minCost = recurseSolver(numPeople, numStops, dests, &lastStop);
+    } 
+    else if (strcmp(mode, "brute") == 0) {
+        minCost = bruteSolver(numPeople, numStops, dests, &lastStop);
+    } 
+    else if (strcmp(mode, "memoize") == 0) {
+        minCost = memoizeSolver(numPeople, numStops, dests, &lastStop);
+    } 
+    else if (strcmp(mode, "dp") == 0) {
+        minCost = dpSolver(numPeople, numStops, dests, &lastStop);
+    } 
+    else {
+        fprintf(stderr, "Unknown mode: %s\n", mode);
+        return 1;
+    }
+
+    printf("Last stop: %d\n", lastStop);
+    printf("Minimum cost: %d\n", minCost);
+
     return 0;
 }
