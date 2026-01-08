9657c68ba8414a503c66fc823542cfaf8efd96c6
gvithoulkas <johnvyth2007@gmail.com>
2026-01-08 22:55:44 +0200

    Update elevate.h

--- elevate/src/elevate.h
+++ elevate/src/elevate.h
@@ -1,1 +1,16 @@
-// γεηααηε
+#ifndef ELEVATE_H
+#define ELEVATE_H
+
+// Recursive solution
+int recurseSolver(int numPeople, int numStops, int dests[], int *lastStop);
+
+// Brute-force solution
+int bruteSolver(int numPeople, int numStops, int dests[], int *lastStop);
+
+// Memoization solution
+int memoizeSolver(int numPeople, int numStops, int dests[], int *lastStop);
+
+// Dynamic programming solution
+int dpSolver(int numPeople, int numStops, int dests[], int *lastStop);
+
+#endif
